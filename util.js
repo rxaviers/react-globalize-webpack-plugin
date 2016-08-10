ca275c36ba02382021ed0ece028bc1607a767ad6
Cecilia <cecilia.mia@gmail.com>
2016-08-10 09:39:11 +0100

    add MS folders support

--- util.js
+++ util.js
@@ -1,6 +1,6 @@
 module.exports = {
   isReactGlobalizeModule: function(filepath) {
-    filepath = filepath.split( "/" );
+    filepath = filepath.split( /[\/\\]/ );
     var i = filepath.lastIndexOf("react-globalize");
     // 1: path should contain "react-globalize",
     // 2: and it should appear between the end (e.g., ../react-globalize) or 2
