87dd22a4476ff44ce05183d119b3ea441eeb107d
Rafael Xavier de Souza <rxaviers@gmail.com>
2021-04-06 19:29:33 -0300

    WIP

--- Extractor.js
+++ Extractor.js
@@ -1,55 +1,61 @@
+"use strict";
+
 /*
  * abstraction for containing extracted phrases from react-globalize-compiler
  */
-var extend = require("util")._extend;
-var reactGlobalizeCompiler = require("react-globalize-compiler");
+const { _extend: extend } = require("util");
+const reactGlobalizeCompiler = require("react-globalize-compiler");
 
-function Extractor() {
-  this.asts = {};
-  this.defaultMessages = {};
-  this.extracts = {};
+class Extractor {
+  constructor() {
+    this.asts = {};
+    this.defaultMessages = {};
+    this.extracts = {};
+  }
+
+  allDefaultMessages() {
+    return Object.keys(this.defaultMessages).reduce((sum, request) => {
+      extend(sum, this.defaultMessages[request]);
+      return sum;
+    }, {});
+  }
+
+  getDefaultMessages(request) {
+    if (!request) {
+      return this.allDefaultMessages();
+    }
+
+    if (!this.defaultMessages[request]) {
+      // Statically extract Globalize & React Globalize default messages.
+      this.defaultMessages[
+        request
+      ] = reactGlobalizeCompiler.extractDefaultMessages(this.asts[request]);
+    }
+
+    return this.defaultMessages[request];
+  }
+
+  allExtracts() {
+    // TODO: Use Object.values()?
+    return Object.keys(this.extracts).map((request) => {
+      return this.extracts[request];
+    });
+  }
+
+  getExtracts(request) {
+    if (!request) {
+      return this.allExtracts();
+    }
+
+    if (!this.extracts[request]) {
+      // Statically extract React Globalize formatters.
+      this.extracts[request] = reactGlobalizeCompiler.extract(
+        this.asts[request]
+      );
+    }
+
+    return this.extracts[request];
+  }
 }
-
-Extractor.prototype.allDefaultMessages = function() {
-  var defaultMessages = this.defaultMessages;
-  return Object.keys(defaultMessages).reduce(function(sum, request) {
-    extend(sum, defaultMessages[request]);
-    return sum;
-  }, {});
-};
-
-Extractor.prototype.getDefaultMessages = function(request) {
-  if (!request) {
-    return this.allDefaultMessages();
-  }
-
-  if (!this.defaultMessages[request]) {
-    // Statically extract Globalize & React Globalize default messages.
-    this.defaultMessages[request] =
-      reactGlobalizeCompiler.extractDefaultMessages(this.asts[request]);
-  }
-
-  return this.defaultMessages[request];
-};
-
-Extractor.prototype.allExtracts = function() {
-  var extracts = this.extracts;
-  return Object.keys(extracts).map(function(request) {
-    return extracts[request];
-  });
-};
-
-Extractor.prototype.getExtracts = function(request) {
-  if (!request) {
-    return this.allExtracts();
-  }
-
-  if (!this.extracts[request]) {
-    // Statically extract React Globalize formatters.
-    this.extracts[request] = reactGlobalizeCompiler.extract(this.asts[request]);
-  }
-
-  return this.extracts[request];
-};
 
 module.exports = Extractor;
--- ProductionModePlugin.js
+++ ProductionModePlugin.js
@@ -1,12 +1,18 @@
-var async = require("async");
-var Extractor = require("./Extractor");
-var fs = require("fs");
-var mkdirp = require("mkdirp");
-var path = require("path");
-var stringify = require("json-stable-stringify");
+"use strict";
+
+const _async = require("async");
+const Extractor = require("./Extractor");
+const fs = require("fs");
+const mkdirp = require("mkdirp");
+const path = require("path");
+const stringify = require("json-stable-stringify");
 
 function alwaysArray(stringOrArray) {
-  return Array.isArray(stringOrArray) ? stringOrArray : stringOrArray ? [stringOrArray] : [];
+  return Array.isArray(stringOrArray)
+    ? stringOrArray
+    : stringOrArray
+    ? [stringOrArray]
+    : [];
 }
 
 function arrayClone(array) {
@@ -36,7 +42,11 @@ function merge() {
   sources.forEach(function(source) {
     var prop;
     for (prop in source) {
-      if (prop in destination && typeof destination[prop] === "object" && !Array.isArray(destination[prop])) {
+      if (
+        prop in destination &&
+        typeof destination[prop] === "object" &&
+        !Array.isArray(destination[prop])
+      ) {
         // Merge Objects
         destination[prop] = merge(destination[prop], source[prop]);
       } else {
@@ -49,8 +59,8 @@ function merge() {
 }
 
 function writeMessagesFile(filepath, data) {
-  process.nextTick(function () {
-    async.series([
+  process.nextTick(function() {
+    _async.series([
       function(callback) {
         mkdirp(path.dirname(filepath), callback);
       },
@@ -61,81 +71,103 @@ function writeMessagesFile(filepath, data) {
   });
 }
 
-function ProductionModePlugin(attributes) {
-  this.attributes = attributes;
-}
+class ProductionModePlugin {
+  constructor(attributes) {
+    this.attributes = attributes;
+  }
 
-ProductionModePlugin.prototype.apply = function(compiler) {
-  var attributes = this.attributes;
-  var developmentLocale = attributes.developmentLocale;
-  var extractor = new Extractor();
-  var messages = attributes.messages;
-  var writeMessages = attributes.writeMessages;
+  apply(compiler) {
+    var attributes = this.attributes;
+    var developmentLocale = attributes.developmentLocale;
+    var extractor = new Extractor();
+    var messages = attributes.messages;
+    var writeMessages = attributes.writeMessages;
 
-  // Map each AST and its request filepath.
-  compiler.parser.plugin("program", function(ast) {
-    extractor.asts[this.state.current.request] = ast;
-  });
+    const bindParser = parser => {
+      // Map each AST and its request filepath.
+      parser.hooks.program.tap("ReactGlobalizePlugin", ast => {
+        extractor.asts[parser.state.current.request] = ast;
+      });
 
-  // Sneaks in modules that `require("react-globalize")` and create custom
-  // precompiled formatters/parsers for them.
-  compiler.parser.plugin("call require:commonjs:item", function(expr, param) {
-    var request = this.state.current.request;
-    if(param.isString() && param.string === "react-globalize") {
-      extractor.getDefaultMessages(request);
-      extractor.getExtracts(request);
-    }
-  });
+      // Sneaks in modules that `require("react-globalize")` and create custom
+      // precompiled formatters/parsers for them.
+      parser.plugin("call require:commonjs:item", function(expr, param) {
+        var request = this.state.current.request;
+        if (param.isString() && param.string === "react-globalize") {
+          extractor.getDefaultMessages(request);
+          extractor.getExtracts(request);
+        }
+      });
+    };
 
-  compiler.plugin("globalize-before-compile-extracts", function(locale, attributes, request) {
-    var originalMessages;
-    var defaultMessages = extractor.getDefaultMessages(request);
-    var extracts = extractor.getExtracts(request);
+    compiler.hooks.globalizeBeforeCompileExtracts.tap(
+      "ReactGlobalizePlugin",
+      (locale, attributes, request) => {
+        const defaultMessages = extractor.getDefaultMessages(request);
+        const extracts = extractor.getExtracts(request);
 
-    if (extracts) {
-      attributes.extracts = attributes.extracts ? arrayClone(alwaysArray(attributes.extracts)) : [];
-      [].push.apply(attributes.extracts, alwaysArray(extracts));
-    }
+        if (extracts) {
+          attributes.extracts = attributes.extracts
+            ? arrayClone(alwaysArray(attributes.extracts))
+            : [];
+          [].push.apply(attributes.extracts, alwaysArray(extracts));
+        }
 
-    // Note react-globalize-compiler actually extracts the default messages for
-    // Globalize and react-globalize <FormatMessage>s. Therefore, the fact
-    // defaultMessages is true means there are messages set either by Globalize
-    // methods or <FormatMessage>.
-    //
-    // If in the future, Globalize supports default messages, probably
-    // globalize-compile will handle the Globalize default messages and
-    // react-globalize-compiler will handle the react-globalize default
-    // messages. Therefore, this code will need changes.
-    if (defaultMessages) {
-      originalMessages = attributes.messages;
+        // Note react-globalize-compiler actually extracts the default messages for
+        // Globalize and react-globalize <FormatMessage>s. Therefore, the fact
+        // defaultMessages is true means there are messages set either by Globalize
+        // methods or <FormatMessage>.
+        //
+        // If in the future, Globalize supports default messages, probably
+        // globalize-compile will handle the Globalize default messages and
+        // react-globalize-compiler will handle the react-globalize default
+        // messages. Therefore, this code will need changes.
+        if (defaultMessages) {
+          const originalMessages = attributes.messages;
 
-      if (locale === developmentLocale) {
-        // Overwrite it with the extracted default messages.
-        attributes.messages = jsonKeyValue(locale, defaultMessages);
-      } else {
-        // Populate the missing messages with the default messages.
-        attributes.messages = merge(
-          jsonKeyValue(locale, defaultMessages),
-          attributes.messages || {}
-        );
-      }
+          if (locale === developmentLocale) {
+            // Overwrite it with the extracted default messages.
+            attributes.messages = jsonKeyValue(locale, defaultMessages);
+          } else {
+            // Populate the missing messages with the default messages.
+            attributes.messages = merge(
+              jsonKeyValue(locale, defaultMessages),
+              attributes.messages || {}
+            );
+          }
 
-      // Write messages if:
-      // 1: `writeMessages` is true, and
-      // 2: processing the whole chunk (i.e., the final i18n bundle where
-      //    `request` is null), and
-      // 3: messages have changed
-      if (writeMessages /*1 */ && !request /* 2 */ && !compareJson(originalMessages, attributes.messages) /* 3 */) {
-        var filepath = messages.replace("[locale]", locale);
-        writeMessagesFile(filepath, attributes.messages);
-        if (locale === developmentLocale) {
-          console.log("Generated `" + filepath + "` using the default translation.");
-        } else {
-          console.log("Populated the new fields of `" + filepath + "` using the default translation.");
+          // Write messages if:
+          // 1: `writeMessages` is true, and
+          // 2: processing the whole chunk (i.e., the final i18n bundle where
+          //    `request` is null), and
+          // 3: messages have changed
+          if (
+            writeMessages /*1 */ &&
+            !request /* 2 */ &&
+            !compareJson(originalMessages, attributes.messages) /* 3 */
+          ) {
+            const filepath = messages.replace("[locale]", locale);
+            writeMessagesFile(filepath, attributes.messages);
+            if (locale === developmentLocale) {
+              console.log(
+                `Generated \`${filepath}\` using the default translation.`
+              );
+            } else {
+              console.log(
+                `Populated the new fields of \`${filepath}\` using the default translation.`
+              );
+            }
+          }
         }
       }
-    }
-  });
-};
+    );
+
+    compiler.hooks.normalModuleFactory.tap("ReactGlobalizePlugin", factory => {
+      factory.hooks.parser
+        .for("javascript/auto")
+        .tap("ReactGlobalizePlugin", bindParser);
+    });
+  }
+}
 
 module.exports = ProductionModePlugin;
